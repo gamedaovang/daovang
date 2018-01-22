c4918d5321c34452a11a7de022e5ffdef9158143
Lamnguyenit <lamnguyen260895@gmail.com>
2018-01-22 12:45:21 +0700

    souce code

---
+++ daovangv3/Assets/Scripts/controllerscrip/mainmenucontroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainmenucontroller : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        Application.LoadLevel ("Level1");
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
